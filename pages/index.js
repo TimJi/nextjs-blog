d42b7fbd53d6036352624c722c7ea70424712a08
Tim Ji <timji@kktv.me>
2021-02-17 00:11:42 +0800

    change title

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>A blog by Tim Ji</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
